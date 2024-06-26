434c5fde3cd9bc0e5d46f78c9c8644512912813d
Santi Villanueva <savife08@gmail.com>
2024-06-26 16:26:18 +0200

    Eliminación de la clase cliente (no se va a usar)

--- client.py
+++
@@ -1,5 +0,0 @@
-import random
-
-class client:
-    def __init__(self):
-        self.id = random.randint(0, 999)
--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from account import account
-from client import client
 
 # Print menu options
 def menu():
+    print('\n')
     print('1. Create account')
     print('2. Delete account')
     print('3. Deposit money')
     print('4. Whithdraw money')
     print('5. Transfer money')
     print('6. Show info')
+    print('7. Exit')
 
 # Funcion principal
 def main():
     menu()
+    option = int(input('Elige una opción: '))
+    if option == 1:
+        print('Opción 1 seleccionada')
+    elif option == 2:
+        print('Opción 2 seleccionada')
+    elif option == 3:
+        print('Opción 3 seleccionada')
+    elif option == 4:
+        print('Opción 4 seleccionada')
+    elif option == 5:
+        print('Opción 5 seleccionada')
+    elif option == 6:
+        print('Opción 6 seleccionada')
+    elif option == 7:
+        print('Opción 7 seleccionada')
+    else:
+        exit()
 
 if __name__ == '__main__':
     main()
